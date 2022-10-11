325778bf3c24cabcf2eb00ea711b167fcf7e1d86
mehedi-hasan-anik <anikm2283@gmail.com>
2022-10-11 20:19:02 +0600

    add photo

--- src/pages/Project/index.jsx
+++ src/pages/Project/index.jsx
@@ -1,16 +1,44 @@
 import React from "react";
 import Tittle from "../../components/Tittle";
-import Project3 from "../../images/Screenshot (48).png";
-import Project4 from "../../images/Screenshot (49).png";
-import Project5 from "../../images/Screenshot (50).png";
-import Project1 from "../../images/Screenshot (81).png";
-import Project2 from "../../images/Screenshot (82).png";
+import Project4 from "../../images/1st.png";
+import Project5 from "../../images/2nd.png";
+import Project1 from "../../images/3rd.png";
+import Project2 from "../../images/4th.png";
+import Project3 from "../../images/5th.png";
+import Project6 from "../../images/Screenshot (49).png";
 import "./Project.styles.css";
 
 const Project = () => {
   const projectDetails = [
     {
       photo: Project1,
+      live: "https://bus365demo.bdtask-demo.com/",
+      details: `To have an opportunity to purchase bus tickets on this website for passengers. Using this
+        website, they will be able to buy tickets by paying online or offline very easily. passengers will
+        get a chance to back the ticket bought within a certain time also`,
+      name: "Bus365",
+    },
+    {
+      photo: Project2,
+      live: "https://foodcorner-7fde5.web.app/",
+      client: "https://github.com/mehedi-hasan-anik/food-recipes",
+      details: `food corner is a recipe exploring website with a clean, simple, and user-friendly UI. At home
+        page, it has a simple introduction and exploring option. visitors can simply change its theme
+        color And can get all the recipe information by clicking the view recipe button. In the top chief
+        section, we can browse all the top chief and their recipe`,
+      name: "FoodCorner",
+    },
+    {
+      photo: Project3,
+      live: "https://restaurant-18eb6.web.app/",
+      client: "https://github.com/mehedi-hasan-anik/restaurant",
+      details: `It's a website based on restaurants. Know about our restaurants. Where you can book a table,
+        order a special menu. Users can create an account via login/registration method`,
+      name: "Gericht",
+    },
+
+    {
+      photo: Project4,
       live: "https://luxury-living-cde62.web.app/",
       client: "https://github.com/mehedi-hasan-anik/lockSmith-client",
       server: "https://github.com/mehedi-hasan-anik/lockSmith-server",
@@ -19,7 +47,7 @@ const Project = () => {
       name: "LockSmith",
     },
     {
-      photo: Project2,
+      photo: Project5,
       live: "https://bookshop-7a3f4.web.app/",
       client: "https://github.com/mehedi-hasan-anik/bookShop-client",
       server: "https://github.com/mehedi-hasan-anik/bookShop-server",
@@ -27,32 +55,15 @@ const Project = () => {
         "This is a kind of ecommerce website. You can buy any kind of book together. As an admin you can add any type of book to this website and you can also delete it.",
       name: "BookShop",
     },
+
     {
-      photo: Project3,
-      live: "https://gallant-beaver-776ef3.netlify.app/",
-      client: "https://github.com/mehedi-hasan-anik/easyRide",
-      server: "",
-      details:
-        "People benefit through this website. You can use this website to go anywhere in Bangladesh very easily and at low cost.",
-      name: "EasyRaide",
-    },
-    {
-      photo: Project4,
+      photo: Project6,
       live: "https://youthful-snyder-bc2c33.netlify.app/",
       client: "https://github.com/mehedi-hasan-anik/premierLeague",
       server: "",
       details:
         "you can get all details of english premier league teams. And this is a single page application.",
       name: "PremierLeague",
-    },
-    {
-      photo: Project5,
-      live: "https://mehedi-hasan-anik.github.io/fancy-slider-main/",
-      client: "https://github.com/mehedi-hasan-anik/fancy-slider-main",
-      server: "",
-      details:
-        "This is a very beautiful website. You can search anything on this website. And you will see many pictures as per search. If you want, you can make slider by selecting some pictures",
-      name: "Photo Gallery",
     },
   ];
 
@@ -63,23 +74,26 @@ const Project = () => {
         <div className="inner_project_wrapper">
           {projectDetails.map((event) => (
             <div className="card-area">
-              <img src={event.photo} alt="" />
-              <h3 className="mt-3">{event.name}</h3>
+              <img src={event?.photo} alt="" />
+              <h3 className="mt-3">{event?.name}</h3>
               <p>{event.details}</p>
               <ul>
                 <li>
-                  <a href={event.live} target="_blank" rel="noreferrer">
+                  <a href={event?.live} target="_blank" rel="noreferrer">
                     Live
                   </a>
                 </li>
-                <li>
-                  <a href={event.client} target="_blank" rel="noreferrer">
-                    client
-                  </a>
-                </li>
-                {event.server && (
+                {event?.client && (
                   <li>
-                    <a href={event.server} target="_blank" rel="noreferrer">
+                    <a href={event?.client} target="_blank" rel="noreferrer">
+                      client
+                    </a>
+                  </li>
+                )}
+
+                {event?.server && (
+                  <li>
+                    <a href={event?.server} target="_blank" rel="noreferrer">
                       server
                     </a>
                   </li>
